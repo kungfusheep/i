b2e4078845aa61a0902723548dbec38a35c3cc8f
Pete Griffiths <pete.griffiths@bet365.com>
2021-04-28 22:49:42 +0100

    init

---
+++ mems.sh
@@ -0,0 +1,36 @@
+
+MEMS_PATH=~/mems/
+
+function mems {
+
+	case "${1}" in
+		"list" )
+			__mems_list; return;;
+	esac
+
+	__mems_write $*
+}
+
+function __mems_write {
+	date > $MEMS_PATH/file
+	git commit -am "$*"
+}
+
+function __mems_list {
+	git --git-dir $MEMS_PATH/.git log --pretty=format:"%Cblue%cr: %Creset%B";
+}
+
+
+if [ ! -e "$MEMS_PATH" ]; then
+	mkdir -p $MEMS_PATH
+	cd $MEMS_PATH
+	git init
+	touch file
+	git add file
+	__mems_write init
+	cd -
+fi
+
+
+
+# git log --pretty=format:"%Cblue%cr: %Creset%B"
